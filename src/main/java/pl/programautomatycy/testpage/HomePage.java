b02a2e85d802914a45d8b502d92f87a2caa7f4a1
EwaPawinska <pawinska.ewa@wp.pl>
2024-11-03 18:06:58 +0100

    Selenium - fluent

--- src/main/java/pl/programautomatycy/testpage/HomePage.java
+++ src/main/java/pl/programautomatycy/testpage/HomePage.java
@@ -1,13 +1,11 @@
 package pl.programautomatycy.testpage;
 
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertTrue;
 
 public class HomePage extends BasePage {
 
